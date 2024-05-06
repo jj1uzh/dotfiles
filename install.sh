78205cc9752f1f5d29314d1adeb483e36da38c38
jj1uzh <git@jj1uzh.net>
2024-05-06 18:53:52 +0900

    hgoe

--- install.sh
+++ install.sh
@@ -1,21 +1,27 @@
-#!/bin/bash -eu
+#!/bin/bash -u
 
 skipped=""
 install_link() {
-    local path="$1"
-    local target="$HOME/$path"
-    echo "==> Install $path"
+    local src_rel="$1"
+    local src_abs=`realpath "$src_rel"`
+    local target="$HOME/$src_rel"
+    echo "==> Install $src_rel"
+    existing_link_target=`readlink -n "$target"`
+    if [ "$existing_link_target" = "$src_abs" ]; then
+        echo " -> $src_rel is already installed."
+	return
+    fi
     if [ -f "$target" ]; then
-        echo " -> ~/$path exists. Skip."
-        skipped="$skipped $path"
+        echo " -> $src_rel exists. Skip."
+        skipped="$skipped $src_rel"
         return
     fi
-    ln -s "./$path" "$target"
+    ln -s "$src_abs" "$target"
 }
 
 install_link ".bashrc"
+install_link ".config/xremap/config.yaml"
 
-echo "Done."
 if [ -n "$skipped" ]; then
-    echo "Skipped: $skipped"
+    echo "\nSkipped: $skipped"
 fi
